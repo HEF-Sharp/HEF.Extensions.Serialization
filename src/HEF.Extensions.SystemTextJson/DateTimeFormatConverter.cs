166ab111dfa96698bf4f9624fda5af5521f4be93
万里涛 <wanlitao_3055601@outlook.com>
2020-04-01 17:53:39 +0800

    添加项目文件。

---
+++ src/HEF.Extensions.SystemTextJson/DateTimeFormatConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HEF.Extensions.SystemTextJson
+{
+    public class DateTimeFormatConverter : JsonConverter<DateTime>
+    {
+        public DateTimeFormatConverter(string formatStr)
+        {
+            if (string.IsNullOrWhiteSpace(formatStr))
+                throw new ArgumentNullException(nameof(formatStr));
+
+            Format = formatStr;
+        }
+
+        protected string Format { get; }
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.GetDateTime();
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(Format));
+        }
+    }
+}
---
+++ src/HEF.Extensions.SystemTextJson/JsonSerializerOptionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HEF.Extensions.SystemTextJson;
+
+namespace System.Text.Json
+{
+    public static class JsonSerializerOptionsExtensions
+    {
+        public static JsonSerializerOptions AddDateTimeFormating(this JsonSerializerOptions options, string formatStr)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.Converters.Add(new DateTimeFormatConverter(formatStr));
+
+            return options;
+        }
+    }
+}
