93beca3f078872d9478924652e4f0882ba8de367
万里涛 <wanlitao_3055601@outlook.com>
2020-08-17 15:47:09 +0800

    add json serializer options extensions

--- src/HEF.Extensions.SystemTextJson/JsonSerializerOptionsExtensions.cs
+++ src/HEF.Extensions.SystemTextJson/JsonSerializerOptionsExtensions.cs
@@ -13,5 +13,35 @@ namespace System.Text.Json
 
             return options;
         }
+
+        public static JsonSerializerOptions SerializeCamelCase(this JsonSerializerOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+            return options;
+        }
+
+        public static JsonSerializerOptions DeserializeCaseInsensitive(this JsonSerializerOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.PropertyNameCaseInsensitive = true;
+
+            return options;
+        }
+
+        public static JsonSerializerOptions IgnoreNullValues(this JsonSerializerOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            options.IgnoreNullValues = true;
+
+            return options;
+        }
     }
 }
